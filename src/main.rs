159294d51b556300cb58255cde801d8d7f74d3aa
phil <uniphil@gmail.com>
2019-02-11 20:41:50 -0500

    hacky request parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-use tiny_http::{Server, Response};
+use tiny_http::{Server, Response, HeaderField};
+use url::{Url};
 
 fn main() {
     let port = match env::var("PORT") {
@@ -9,12 +10,33 @@ fn main() {
 
     let server = Server::http(("0.0.0.0", port)).unwrap();
 
+    let mut count = 0;
+
+    let referer_header = HeaderField::from_bytes("Referer").unwrap();
+
     for request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
-            request.method(),
-            request.url(),
-            request.headers()
-        );
+        count += 1;
+        println!("req n {:?}", count);
+        let referer = request.headers()
+            .iter()
+            .find(|header| header.field == referer_header)
+            .map(|header| Url::parse(header.value.as_str()));
+        let url = match referer {
+            Some(r) => match r {
+                Err(e) => {
+                    println!("bad referer: {:?}", e);
+                    continue;
+                },
+                Ok(d) => d,
+            },
+            None => {
+                println!("no referrer");
+                continue;
+            }
+        };
+
+        println!("host: {:?}", url.host_str().unwrap());
+        println!("path: {:?}", url.path());
 
         let response = Response::from_string("hello world");
         request.respond(response).unwrap();
