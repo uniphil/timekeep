7eaeb6784c078b76aff646cb393143f7a303c62d
phil <uniphil@gmail.com>
2019-02-11 21:00:39 -0500

    horrible code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use tiny_http::{Server, Response, HeaderField};
+use std::collections::HashMap;
 use url::{Url};
+use tiny_http::{Server, Response, HeaderField};
 
 fn main() {
     let port = match env::var("PORT") {
@@ -10,13 +11,11 @@ fn main() {
 
     let server = Server::http(("0.0.0.0", port)).unwrap();
 
-    let mut count = 0;
+    let mut by_parts: HashMap<String, HashMap<String, u32>> = HashMap::new();
 
     let referer_header = HeaderField::from_bytes("Referer").unwrap();
 
     for request in server.incoming_requests() {
-        count += 1;
-        println!("req n {:?}", count);
         let referer = request.headers()
             .iter()
             .find(|header| header.field == referer_header)
@@ -35,8 +34,22 @@ fn main() {
             }
         };
 
-        println!("host: {:?}", url.host_str().unwrap());
-        println!("path: {:?}", url.path());
+        let host = match url.host_str() {
+            Some(h) => h,
+            None => {
+                println!("could not parse host :(");
+                continue;
+            }
+        };
+
+        let path = url.path();
+
+        let counter = by_parts
+            .entry(host.to_string()).or_insert(HashMap::new())
+            .entry(path.to_string()).or_insert(0);
+        *counter += 1;
+
+        println!("{} – {} {}", counter, host, path);
 
         let response = Response::from_string("hello world");
         request.respond(response).unwrap();
